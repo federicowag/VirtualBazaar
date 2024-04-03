4207a01a384cc7e15a752a5a9845c15a0d6b0704
federicowag <federicowagner7@gmail.com>
2024-04-02 23:29:13 -0300

    Modificaciones en JS

---
+++ js/cart.js
@@ -0,0 +1,91 @@
+let cart = [];
+
+function loadCart() {
+    const cartData = localStorage.getItem('cart');
+    if (cartData) {
+        cart = JSON.parse(cartData);
+        displayCart();
+    }
+}
+
+function displayCart() {
+    const cartContainer = document.getElementById('cart-container');
+    cartContainer.innerHTML = '';
+
+    cart.forEach((item, index) => {
+        const productTotal = item.price * item.quantity;
+
+        const cartItem = `
+            <div class="cart-item">
+                <div class="cart-item-name">${item.name}</div>
+                <div class="cart-item-quantity">Cantidad: ${item.quantity}</div>
+                <div class="cart-item-price">Precio por artículo: $${item.price}</div>
+                <div class="cart-item-total">Total por producto: $${productTotal.toFixed(2)}</div>
+                <button onclick="removeFromCart(${index})">Eliminar</button>
+            </div>
+        `;
+
+        cartContainer.innerHTML += cartItem;
+    });
+
+    calculateTotal();
+}
+
+
+function calculateTotal() {
+    let cartTotal = 0;
+    cart.forEach(item => {
+        const productTotal = item.price * item.quantity;
+        cartTotal += productTotal;
+    });
+
+    const totalElement = document.getElementById('cart-total');
+    totalElement.textContent = `Total: $${cartTotal.toFixed(2)}`;
+}
+
+
+function addToCart(name, price, image) {
+    let productExists = false;
+
+    cart.forEach(item => {
+        if (item.name === name) {
+            item.quantity++;
+            productExists = true;
+        }
+    });
+
+    if (!productExists) {
+        cart.push({
+            name: name,
+            price: price,
+            image: image,
+            quantity: 1
+        });
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    displayCart();
+}
+
+
+function removeFromCart(index) {
+    cart.splice(index, 1);
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    displayCart();
+}
+
+
+function checkout() {
+
+    cart = [];
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    displayCart();
+
+    alert('¡Gracias por su compra! 😃');
+}
+
+loadCart();
--- js/main.js
+++ js/main.js
@@ -1,1 +1,59 @@
-addEventListener
+const products = [
+    {
+        name: 'Red Dead Redemption 2',
+        price: 50000,
+        image: './img/images.jpeg',
+        description: 'Red Dead Redemption 2 es un épico juego de acción y aventura...'
+    },
+    {
+        name: 'Dark Souls III',
+        price: 44000,
+        image: './img/dark-souls-3.jpg',
+        description: 'Dark Souls III es un desafiante juego de acción y rol...'
+    },
+    {
+        name: 'Gears 5',
+        price: 35000,
+        image: './img/gerrr.jpg',
+        description: 'Gears 5 es un juego de acción en tercera persona...'
+    },
+    {
+        name: 'Half Life Alyx',
+        price: 60000,
+        image: './img/half life.jpeg',
+        description: 'Half-Life: Alyx es un juego de realidad virtual...'
+    },
+    {
+        name: 'Resident Evil 2 Remake',
+        price: 20000,
+        image: './img/residentevil2-featured-1024x597.png',
+        description: 'Resident Evil 2 Remake es una reimaginación del clásico juego...'
+    },
+    {
+        name: 'The Witcher 3: Wild Hunt',
+        price: 18500,
+        image: './img/witcher.jpeg',
+        description: 'The Witcher 3: Wild Hunt es un aclamado juego de rol...'
+    }
+];
+
+function displayProducts() {
+    const productContainer = document.getElementById('product-container');
+    productContainer.innerHTML = '';
+
+    products.forEach(product => {
+        const productElement = `
+            <section class="product">
+                <img src="${product.image}" alt="${product.name}">
+                <h2>${product.name}</h2>
+                <p>${product.description}</p>
+                <span>$${product.price}</span>
+                <button onclick="addToCart('${product.name}', ${product.price}, '${product.image}')">Comprar ahora</button>
+            </section>
+        `;
+
+        productContainer.innerHTML += productElement;
+    });
+}
+
+displayProducts();
